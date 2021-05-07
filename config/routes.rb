b05a60fccee86f1f0a941a147867cce7dd1edfa0
takashi-Q <79442863+takashi-Q@users.noreply.github.com>
2021-05-07 18:25:59 +0900

    デプロイの為の変更？

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   get 'messages/new'
   get 'musics/index'
   get 'musics/search'
-  root to:'musics#index'
+  root 'musics#index'
   resources :messages,only: [:new,:create]
 end
