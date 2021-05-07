70011a63b0b529a6e1ccbfe619f13587f9c1a027
takashi-Q <79442863+takashi-Q@users.noreply.github.com>
2021-05-07 18:16:12 +0900

    追記

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'messages/new'
+  get 'musics/index'
+  get 'musics/search'
+  root to:'musics#index'
+  resources :messages,only: [:new,:create]
 end
